f54267d41fd58b87a567e626929f0d710cebc8b3
Colin Anderson <andercol.4@gmail.com>
2018-11-04 13:32:50 -0700

    split up the app.rb into models files

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 require 'gosu'
+require 'pry'
+Dir[File.join(__dir__, 'models', '*.rb')].each { |file| require file }
+
+module ZOrder
+  BACKGROUND, OBSTICALS, PLAYER, UI = *0..3
+end
+
 
 class Tutorial < Gosu::Window
   def initialize
@@ -6,13 +13,55 @@ class Tutorial < Gosu::Window
     self.caption = "Tutorial Game"
 
     @background_image = Gosu::Image.new("media/space.png", :tileable => true)
+
+    @player = Player.new
+    @player.warp(320, 240)
+
+    @star_anim = Gosu::Image.load_tiles("media/star.png", 25, 25)
+    @stars = Array.new
+    @asteroids = Array.new
+    @font = Gosu::Font.new(20)
+
   end
 
   def update
+    if Gosu.button_down? Gosu::KB_LEFT or Gosu::button_down? Gosu::GP_LEFT
+      @player.turn_left
+    end
+    if Gosu.button_down? Gosu::KB_RIGHT or Gosu::button_down? Gosu::GP_RIGHT
+      @player.turn_right
+    end
+    if Gosu.button_down? Gosu::KB_UP or Gosu::button_down? Gosu::GP_BUTTON_0
+      @player.accelerate
+    end
+    @player.move
+    @player.collect_stars(@stars)
+
+    if rand(100) < 4 && @stars.size < 25
+      @stars.push(Star.new(@star_anim))
+    end
+
+    if rand(100) < 4 && @asteroids.size < 5
+      @asteroids.push(Asteroid.new)
+    end
+
   end
 
   def draw
-    @background_image.draw(0, 0, 0)
+    @background_image.draw(0, 0, ZOrder::BACKGROUND)
+    @player.draw
+    @stars.each { |star| star.draw }
+    @asteroids.each { |asteroid| asteroid.draw }
+    @font.draw_text("Score: #{@player.score}", 10, 10, ZOrder::UI, 1.0, 1.0, Gosu::Color::YELLOW)
+
+  end
+
+  def button_down(id)
+    if id == Gosu::KB_ESCAPE
+      close
+    else
+      super
+    end
   end
 end
 
